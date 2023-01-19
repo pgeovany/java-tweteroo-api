6d2461bd72d4f540f9cbccbe2f79eff66131c23c
Pablo Souza <pgeovany14@gmail.com>
2023-01-18 23:14:33 -0300

    feat: implement a constructor in the User model

--- src/main/java/com/tweteroo/api/model/User.java
+++ src/main/java/com/tweteroo/api/model/User.java
@@ -1,5 +1,7 @@
 package com.tweteroo.api.model;
 
+import com.tweteroo.api.dto.UserDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,11 +9,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
 public class User {
+
+  public User(UserDTO data) {
+    this.username = data.username();
+    this.avatar = data.avatar();
+  }
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
