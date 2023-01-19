a4ccdcb8fc8ffc1d8298591d697ed143ecb4d11c
Pablo Souza <pgeovany14@gmail.com>
2023-01-18 22:54:32 -0300

    feat: implement User model

---
+++ src/main/java/com/tweteroo/model/User.java
@@ -0,0 +1,25 @@
+package com.tweteroo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(length = 30, nullable = false, unique = true)
+  private String username;
+
+  @Column(nullable = false)
+  private String avatar;
+}
