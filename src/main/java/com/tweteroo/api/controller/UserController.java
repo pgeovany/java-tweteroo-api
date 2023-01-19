7d90eea68c63df711b2ce490755bc7d22ba1c4b8
Pablo Souza <pgeovany14@gmail.com>
2023-01-18 23:15:09 -0300

    feat: implement a function to create a new user

--- src/main/java/com/tweteroo/api/controller/UserController.java
+++ src/main/java/com/tweteroo/api/controller/UserController.java
@@ -1,10 +1,24 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.dto.UserDTO;
+import com.tweteroo.api.model.User;
+import com.tweteroo.api.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-  
+ 
+  @Autowired
+  private UserRepository repository;
+
+  @PostMapping
+  public void create(@RequestBody UserDTO request) {
+    repository.save(new User(request));
+  }
 }
